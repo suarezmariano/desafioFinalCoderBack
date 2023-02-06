f8f00ad463e9a6fa9329212a6c0dea773216c76f
suarezmariano <marianosuarez@hotmail.com>
2023-02-05 23:47:28 -0300

    agregado de add y delete products to cart

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -84,6 +84,66 @@ class cartsController {
       return { error: -8, descripcion: 'No se pudo borrar el carrito' };
     }
   }
+
+  async addProductToCart(id, p) {
+    const carts = await this.getCarts();
+    const index = carts.findIndex((c) => c.id === id);
+    const newProd = p;
+
+    if (index === -1) {
+      throw { error: -5, descripcion: `No existe el carrito con ID ${id}` };
+    }
+
+    const cartProducts = carts[index].productos;
+    const cartProdLength = cartProducts.length;
+
+    if (cartProdLength > 0) {
+      newProd.id = cartProducts[cartProdLength - 1].id + 1;
+    } else {
+      newProd.id = 1;
+    }
+    newProd.timestamp = Date.now();
+
+    cartProducts.push(newProd);
+
+    try {
+      await fs.promises.writeFile(this.path, JSON.stringify(carts, null, 2));
+      return newProd;
+    } catch {
+      throw {
+        error: -7,
+        descripcion: 'No se pudo modificar el carrito de productos',
+      };
+    }
+  }
+
+  async deleteProductFromCart(cartId, prodId) {
+    const carts = await this.getCarts();
+    const index = carts.findIndex((c) => c.id === cartId);
+
+    if (index === -1) {
+      throw { error: -5, descripcion: `No existe el carrito de id ${cartId}` };
+    }
+
+    const cartProducts = carts[index].productos;
+    const prod = cartProducts.find((p) => p.id === prodId);
+
+    if (!prod) {
+      throw { error: -5, descripcion: `No existe el producto de id ${prodId}` };
+    }
+
+    carts[index].productos = cartProducts.filter((p) => p.id !== prodId);
+
+    try {
+      await fs.promises.writeFile(this.path, JSON.stringify(carts, null, 2));
+      return prod;
+    } catch {
+      throw {
+        error: -7,
+        descripcion: 'No se pudo modificar el carrito de productos',
+      };
+    }
+  }
 }
 
 module.exports = cartsController;
